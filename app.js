ecc2ab80cd6299c6a17b20ce210d66ddf3afacdc
Nasir Ahmad <najarnasir707@gmail.com>
2026-01-08 21:57:42 +0530

    Prepare Blogify for Vercel deployment

--- app.js
+++ app.js
@@ -5,33 +5,37 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 
 const Blog = require("./models/blog");
-
 const userRoute = require("./routes/user");
 const blogRoute = require("./routes/blog");
-
 const {
   checkForAuthenticationCookie,
 } = require("./middlewares/authentication");
 
 const app = express();
-const PORT = process.env.MONGO_URL || 8001;
 
-
-// mongoose
-//   .connect(process.env.MONGODB_URL)
-//   .then((e) => console.log("MongoDB Connected"))
-// ;
-
+// MongoDB connection (safe for Vercel)
+let isConnected = false;
+async function connectDB() {
+  if (isConnected) return;
+  await mongoose.connect(process.env.MONGO_URL);
+  isConnected = true;
+  console.log("MongoDB Connected");
+}
+connectDB();
 
 mongoose.set("strictQuery", true);
+
+// View engine
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+// Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
 app.use(express.static(path.resolve("./public")));
 
+// Routes
 app.get("/", async (req, res) => {
   const allBlogs = await Blog.find({});
   res.render("home", {
@@ -43,4 +47,5 @@ app.get("/", async (req, res) => {
 app.use("/user", userRoute);
 app.use("/blog", blogRoute);
 
-app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
+// ❌ NO app.listen here
+module.exports = app;
